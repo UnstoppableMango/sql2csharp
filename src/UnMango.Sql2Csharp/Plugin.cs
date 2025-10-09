7957f395bd292ca0be3ace8103a4c6ddb40aab10
Erik Rasmussen <erik.rasmussen@unmango.dev>
2025-10-08 21:50:53 -0500

    WIP

--- src/UnMango.Sql2Csharp/Plugin.cs
+++ src/UnMango.Sql2Csharp/Plugin.cs
@@ -7,5 +7,8 @@ public static class Plugin
 	public static ValueTask<int> ExecuteAsync(IReadOnlyList<string> args, CancellationToken cancellationToken = default)
 	{
 		var parser = new SqlQueryParser();
+		var seq = parser.Parse(args[0]);
+
+		return new(0);
 	}
 }
